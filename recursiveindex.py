caadc06305153b2aec6d86ad4d13368d5ddc244f
Dominique Cuevas <cuevasdominique@gmail.com>
2020-01-13 16:17:03 -0800

    find index recursively

---
+++ recursiveindex.py
@@ -0,0 +1,44 @@
+"""Given list (haystack), return index (0-based) of needle in the list.
+
+    Return None if needle is not in haystack.
+
+    Do this with recursion. You MAY NOT USE A `for` OR `while` LOOP.
+
+For example:
+
+    >>> recursive_index(5, [1, 3, 5, 2, 4])
+    2
+
+    >>> recursive_index("hey", ["hey", "there", "you"])
+    0
+
+    >>> recursive_index("you", ["hey", "there", "you"])
+    2
+
+    >>> recursive_index("zork", ["foo", "bar", "baz"]) is None
+    True
+
+"""
+
+
+def recursive_index(needle, haystack):
+    """Given list (haystack), return index (0-based) of needle in the list.
+
+    Return None if needle is not in haystack.
+
+    Do this with recursion. You MAY NOT USE A `for` OR `while` LOOP.
+    """
+    # nesting another function to keep start idx at 0
+    def _recursive_index(needle, haystack, idx):
+        if len(haystack) == idx:
+            return
+        if haystack[idx] == needle:
+            return idx
+        return _recursive_index(needle, haystack, idx +1)
+    return _recursive_index(needle, haystack, 0)
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED. GO GO GO!\n")
